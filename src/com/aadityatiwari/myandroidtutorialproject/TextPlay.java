357f0f7ce1a611a62d6097fd674eec66894a94cd
Aaditya Tiwari <aadi.dmw@gmail.com>
2014-03-24 02:09:57 +0530

    Till 34th tutorial
    implements View.OnClickListener's onClick() and based on objects's ID
    retrieved from view, set different text related properties

--- src/com/aadityatiwari/myandroidtutorialproject/TextPlay.java
+++ src/com/aadityatiwari/myandroidtutorialproject/TextPlay.java
@@ -1,8 +1,13 @@
 package com.aadityatiwari.myandroidtutorialproject;
 
+import java.util.Random;
+
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter.ViewBinder;
 import android.text.InputType;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -10,30 +15,86 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-public class TextPlay extends Activity {
+public class TextPlay extends Activity implements View.OnClickListener {
+
+	Button checkCommandButton;
+	ToggleButton toggleButton;
+	EditText inputEditText;
+	TextView displayTextView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.text);
+		initVariables();
+		toggleButton.setOnClickListener(this);
+		checkCommandButton.setOnClickListener(this);
 
-		Button checkCommandButton = (Button) findViewById(R.id.bResults);
-		final ToggleButton toggleButton = (ToggleButton) findViewById(R.id.tbPassword);
-		final EditText inputEditText = (EditText) findViewById(R.id.etCommands);
-		TextView displayTextView = (TextView) findViewById(R.id.tvResults);
-		toggleButton.setOnClickListener(new OnClickListener() {
+	}
 
-			@Override
-			public void onClick(View v) {
-				if(toggleButton.isChecked()){
-					inputEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+	private void initVariables() {
+		checkCommandButton = (Button) findViewById(R.id.bResults);
+		toggleButton = (ToggleButton) findViewById(R.id.tbPassword);
+		inputEditText = (EditText) findViewById(R.id.etCommands);
+		displayTextView = (TextView) findViewById(R.id.tvResults);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+		switch (v.getId()) {
+		case R.id.bResults:
+			/* checkCommandButton OnClickListener section -- BEGINS */
+			String check = inputEditText.getText().toString();
+			if (check.equals("left")) {
+				displayTextView.setGravity(Gravity.LEFT);
+			} else if (check.equals("center")) {
+				displayTextView.setGravity(Gravity.CENTER);
+			} else if (check.equals("right")) {
+				displayTextView.setGravity(Gravity.RIGHT);
+			} else if (check.equals("blue")) {
+				displayTextView.setGravity(Gravity.CENTER);
+			} else if (check.equals("random")) {
+				Random r = new Random();
+				displayTextView.setText("< Random >");
+				displayTextView.setTextSize(r.nextInt(75));
+				displayTextView.setTextColor(Color.rgb(r.nextInt(255),
+						r.nextInt(255), r.nextInt(255)));
+				displayTextView.setGravity(Gravity.CENTER);
+				switch (r.nextInt(3)) {
+				case 0:
+					displayTextView.setGravity(Gravity.LEFT);
+					break;
+				case 1:
+					displayTextView.setGravity(Gravity.CENTER);
+					break;
+				case 2:
+					displayTextView.setGravity(Gravity.RIGHT);
+					break;
+				default:
 				}
-				else {
-					inputEditText.setInputType(InputType.TYPE_CLASS_TEXT);
-				}
+			} else {
+				displayTextView.setText("invalid");
+				displayTextView.setGravity(Gravity.CENTER);
+				displayTextView.setTextColor(Color.RED);
 			}
-		});
+			/* checkCommandButton OnClickListener section -- ENDS */
+			break;
+		case R.id.tbPassword:
+			/* toggleButton OnClickListener section -- BEGINS */
+			if (toggleButton.isChecked()) {
+				inputEditText.setInputType(InputType.TYPE_CLASS_TEXT
+						| InputType.TYPE_TEXT_VARIATION_PASSWORD);
+			} else {
+				inputEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+			}
+			/* toggleButton OnClickListener section -- ENDS */
+			break;
+		default:
+
+		}
 
 	}
 
