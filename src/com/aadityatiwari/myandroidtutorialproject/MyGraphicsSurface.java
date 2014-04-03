ddb1a3a1bda05b5bc1d988d0496bc559954264a6
Aaditya Tiwari <aadi.dmw@gmail.com>
2014-04-04 02:11:55 +0530

    Added logic to trace back the ball from point B to point A

--- src/com/aadityatiwari/myandroidtutorialproject/MyGraphicsSurface.java
+++ src/com/aadityatiwari/myandroidtutorialproject/MyGraphicsSurface.java
@@ -15,9 +15,9 @@ import android.view.View.OnTouchListener;
 public class MyGraphicsSurface extends Activity implements OnTouchListener {
 
 	MyAnimationSurfaceView myAnimationSurfaceView;
-	float x, y, sX, sY, fX, fY;
-	Bitmap bmS, bmF, test;
+	float x, y, sX, sY, fX, fY; // S => start ; f => finish
 	float dX, dY, aniX, aniY, scaledX, scaledY;
+	Bitmap redBall, aSign, bSign;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +31,11 @@ public class MyGraphicsSurface extends Activity implements OnTouchListener {
 		sY = 0;
 		fX = 0;
 		fY = 0;
+		dX = dY = aniX = aniY = scaledX = scaledY = 0;
+		redBall = BitmapFactory.decodeResource(getResources(),
+				R.drawable.red_ball);
+		aSign = BitmapFactory.decodeResource(getResources(), R.drawable.a);
+		bSign = BitmapFactory.decodeResource(getResources(), R.drawable.b);
 		setContentView(myAnimationSurfaceView);
 	}
 
@@ -51,7 +56,24 @@ public class MyGraphicsSurface extends Activity implements OnTouchListener {
 		// TODO Auto-generated method stub
 		x = event.getX();
 		y = event.getY();
-		return false;
+
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			sX = event.getX();
+			sY = event.getY();
+			dX = dY = aniX = aniY = scaledX = scaledY = fX = fY = 0;
+			break;
+		case MotionEvent.ACTION_UP:
+			fX = event.getX();
+			fY = event.getY();
+			dX = fX - sX;
+			dY = fY - sY;
+			scaledX = dX / 30;
+			scaledY = dY / 30;
+			x = y = 0;
+			break;
+		}
+		return true;
 	}
 
 	public class MyAnimationSurfaceView extends SurfaceView implements Runnable {
@@ -75,10 +97,24 @@ public class MyGraphicsSurface extends Activity implements OnTouchListener {
 				canvas.drawRGB(240, 240, 240);
 
 				if (x != 0 && y != 0) {
-					Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.red_ball);
-					canvas.drawBitmap(bmp, x, y, null);
-					
+					canvas.drawBitmap(redBall, (x - redBall.getWidth() / 2),
+							(y - redBall.getHeight() / 2), null);
 				}
+
+				if (sX != 0 && sY != 0) {
+					canvas.drawBitmap(aSign, sX - (aSign.getWidth() / 2), sY
+							- (aSign.getHeight() / 2), null);
+				}
+				if (fX != 0 && fY != 0) {
+
+					canvas.drawBitmap(redBall, (fX - redBall.getWidth() / 2)
+							- aniX, (fY - redBall.getHeight() / 2) - aniY, null);
+					canvas.drawBitmap(bSign, fX - (bSign.getWidth() / 2), fY
+							- (bSign.getHeight() / 2), null);
+				}
+				aniX = aniX + scaledX;
+				aniY = aniY + scaledY;
+
 				ourSurfaceHolder.unlockCanvasAndPost(canvas);
 			}
 		}
