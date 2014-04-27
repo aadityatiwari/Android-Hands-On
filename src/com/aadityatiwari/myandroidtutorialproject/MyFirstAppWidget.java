7f6f78885e97fa28d7cc54ec90dd5fd3642579bb
Aaditya Tiwari <aadi.dmw@gmail.com>
2014-04-27 21:38:48 +0530

    First Android Widget

--- src/com/aadityatiwari/myandroidtutorialproject/MyFirstAppWidget.java
+++ src/com/aadityatiwari/myandroidtutorialproject/MyFirstAppWidget.java
@@ -1,5 +1,38 @@
 package com.aadityatiwari.myandroidtutorialproject;
 
-public class MyFirstAppWidget {
+import java.util.Random;
 
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+public class MyFirstAppWidget extends AppWidgetProvider {
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		Random r = new Random();
+		int randomInt = r.nextInt(100000000);
+		String rand = String.valueOf(randomInt);
+
+		final int N = appWidgetIds.length;
+
+		for (int i = 0; i < N; i++) {
+			int awID = appWidgetIds[i];
+			RemoteViews v = new RemoteViews(context.getPackageName(),
+					R.layout.first_widget);
+			v.setTextViewText(R.id.tvwidgetUpdate, rand);
+			appWidgetManager.updateAppWidget(awID, v);
+		}
+
+	}
+
+	@Override
+	public void onDeleted(Context context, int[] appWidgetIds) {
+		super.onDeleted(context, appWidgetIds);
+		Toast.makeText(context, "Widget deleted", Toast.LENGTH_SHORT).show();
+	}
 }
--- src/com/aadityatiwari/myandroidtutorialproject/MyFirstAppWidgetConfig.java
+++ src/com/aadityatiwari/myandroidtutorialproject/MyFirstAppWidgetConfig.java
@@ -1,5 +1,35 @@
 package com.aadityatiwari.myandroidtutorialproject;
 
-public class MyFirstAppWidgetConfig {
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MyFirstAppWidgetConfig extends Activity implements OnClickListener {
+
+	EditText info;
+	AppWidgetManager awm;
+	Context c;
+	int awID;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.first_widgetconfig);
+		Button b = (Button) findViewById(R.id.bwidgetconfig);
+		b.setOnClickListener(this);
+		c = MyFirstAppWidgetConfig.this;
+		info = (EditText) findViewById(R.id.etwidgetconfig);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+
+	}
 
 }
