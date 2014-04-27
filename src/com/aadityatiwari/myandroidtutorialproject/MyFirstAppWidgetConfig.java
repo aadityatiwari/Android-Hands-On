6d47336fba9a6e61462c2af24ac34faba718b607
Aaditya Tiwari <aadi.dmw@gmail.com>
2014-04-27 22:05:00 +0530

    Usage of AppWidgetManager to get the extras from the calling widget in the called Activity
    AppWidgetManager.EXTRA_APPWIDGET_ID

--- src/com/aadityatiwari/myandroidtutorialproject/MyFirstAppWidgetConfig.java
+++ src/com/aadityatiwari/myandroidtutorialproject/MyFirstAppWidgetConfig.java
@@ -3,11 +3,13 @@ package com.aadityatiwari.myandroidtutorialproject;
 import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RemoteViews;
 
 public class MyFirstAppWidgetConfig extends Activity implements OnClickListener {
 
@@ -25,10 +27,28 @@ public class MyFirstAppWidgetConfig extends Activity implements OnClickListener 
 		c = MyFirstAppWidgetConfig.this;
 		info = (EditText) findViewById(R.id.etwidgetconfig);
 
+		// Getting info about the widget that Launched this Activity
+		Intent i = getIntent();
+		Bundle extras = i.getExtras();
+		if (extras != null) {
+			awID = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,
+					AppWidgetManager.INVALID_APPWIDGET_ID);
+		} else {
+			finish();
+		}
+		awm = AppWidgetManager.getInstance(c);
 	}
 
 	@Override
 	public void onClick(View v) {
+		String textFromWidgetConfigTextView = info.getText().toString();
+		RemoteViews views = new RemoteViews(c.getPackageCodePath(),
+				R.layout.first_widget);
+		// Set textFromWidgetConfigTextView into the widget TextView field
+		views.setTextViewText(R.id.tvConfigInput, textFromWidgetConfigTextView);
+		awm.updateAppWidget(awID, views);
+
+		finish();
 
 	}
 
