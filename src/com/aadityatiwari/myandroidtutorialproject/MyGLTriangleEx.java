28c9e627c329b9fd41a39ccfb2e5fd61d11c755b
Aaditya Tiwari <aadi.dmw@gmail.com>
2014-05-02 23:52:17 +0530

    Learning OpenGL

--- src/com/aadityatiwari/myandroidtutorialproject/MyGLTriangleEx.java
+++ src/com/aadityatiwari/myandroidtutorialproject/MyGLTriangleEx.java
@@ -3,6 +3,9 @@ package com.aadityatiwari.myandroidtutorialproject;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
 
 public class MyGLTriangleEx {
 
@@ -10,14 +13,34 @@ public class MyGLTriangleEx {
 			1.0f, -1.0f, // p1
 			-1.0f, -1.0f // p2
 	};
+	private short[] pIndex = { 0, 1, 2 };
 
 	private FloatBuffer vertBuff, colorBuff;
+	private ShortBuffer pBuff;
 
 	public MyGLTriangleEx() {
 
 		ByteBuffer bBuff = ByteBuffer.allocateDirect(vertices.length * 4);
 		bBuff.order(ByteOrder.nativeOrder());
 		vertBuff = bBuff.asFloatBuffer();
+		vertBuff.put(vertices);
+		vertBuff.position(0);
+
+		ByteBuffer pbBuff = ByteBuffer.allocateDirect(pIndex.length * 2);
+		pbBuff.order(ByteOrder.nativeOrder());
+		pBuff = pbBuff.asShortBuffer();
+		pBuff.put(pIndex);
+		pBuff.position(0);
+
+	}
+
+	public void draw(GL10 gl) {
+		gl.glFrontFace(GL10.GL_CW);
+		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+		gl.glVertexPointer(2, GL10.GL_FLOAT, 0, vertBuff);
+		gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+		
+		
 	}
 
 }
